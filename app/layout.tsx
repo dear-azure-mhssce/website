cc5d06c8a59ea006863a16f8a333c5f0aea4db7d
Shaikh Mohammed Umair <mohumair1901@gmail.com>
2024-08-27 00:27:17 +0530

    color change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Hero from "@/components/Hero";
-
+import AboutUs from "@/components/AboutUs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Hero />
+        <AboutUs />
       </body>
     </html>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { BackgroundBeamsWithCollision } from "./ui/BgBeam";
+import { TextGenerateEffect } from "./ui/TextGenerate";
 
 const Hero = () => {
   return (
     <BackgroundBeamsWithCollision>
-      <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
-        Welcome to{" "}
-        <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-          <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-            <span className="">Dear Azure MHSSCE</span>
-          </div>
-          <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-            <span className="">Dear Azure MHSSCE</span>
-          </div>
-        </div>
-      </h2>
+      <TextGenerateEffect
+        className="text-left md:text-5xl lg:text-6xl"
+        words="Welcome to Dear Azure"
+      />
     </BackgroundBeamsWithCollision>
   );
 };
